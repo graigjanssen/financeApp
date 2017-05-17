32995565e3ed1e65445c5fc37a645943d08f9573
Graig Janssen <graig.janssen@gmail.com>
2017-05-17 17:25:02 -0400

    Initialize store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { operationsReducer } from './common/operations';
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore({ operations: operationsReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
